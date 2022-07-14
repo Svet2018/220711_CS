3dfaa5dd740040223fba1f58dc142689d7ac7d64
Svet2018 <bulatova44@gmail.com>
2022-07-14 21:06:36 +0300

    Added information about the rules for creating an array

--- 220714 _Arrays_cs/Program.cs
+++ 220714 _Arrays_cs/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Создание массива целых чисел разными способами:
+
+int[] array1;
+array1 = new int[10];
+
+int[] array2 = new int[5];
+
+int[] array3 = new int[5] {1, 2, 3, 4, 5};
+
+int[] array4 = {3, 4, 5, 6, 7, 8, 9};
+
+Array array5 = new int[7];
+
+Array array6 = Array.CreateInstance(typeof(int), 5);
+
+
+
+
+
+
+
