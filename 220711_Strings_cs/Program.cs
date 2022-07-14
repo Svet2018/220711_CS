41f429322216a5b68d21365fb34e2692668d5c2f
Svet2018 <bulatova44@gmail.com>
2022-07-14 20:46:10 +0300

    Created a new folder - 220714_Array_cs

--- 220711_Strings_cs/Program.cs
+++ 220711_Strings_cs/Program.cs
@@ -30,3 +30,42 @@ System.Console.WriteLine(substr);
 
 string substri = name.Substring(0, 3); // Взятие подстроки щт 0 до 3 индекса
 System.Console.WriteLine(substri);
+
+System.Console.WriteLine("Проверка на пустоту");
+string empty = "";
+string nullstring = null;
+bool isnulempty = string.IsNullOrEmpty(empty);
+bool isnulempty1 = string.IsNullOrEmpty(nullstring);
+System.Console.WriteLine(isnulempty);
+System.Console.WriteLine(isnulempty1);
+
+System.Console.WriteLine("Проверка на пустоту");
+string backsp = " ";
+bool isnulempty2 = string.IsNullOrWhiteSpace(backsp);
+System.Console.WriteLine(isnulempty2);
+
+
+//Объединение строк
+
+string str = string.Concat("My ", "name ", "is ", "John.");
+System.Console.WriteLine(str);
+
+string str1 = string.Join(" ", "My", "name", "is", "John.");
+System.Console.WriteLine(str1);
+
+str1 = str1.Insert(0, "By the way, "); //Вставляет подстроку начиная с 0 индекса
+System.Console.WriteLine(str1);
+
+str1 = str1.Remove(0, 3); //Удаляет символы с 0 по 3 индекс подстроки 
+System.Console.WriteLine(str1);
+
+string str2 = str.Replace('n', 'k'); //Заменяет все символы n на k
+System.Console.WriteLine(str2);
+
+string data = "1;2;3;4;5;6;7;8;9";
+string[] splitdata = data.Split(';');
+string first = splitdata[3];
+System.Console.WriteLine(first);
+
+
+
